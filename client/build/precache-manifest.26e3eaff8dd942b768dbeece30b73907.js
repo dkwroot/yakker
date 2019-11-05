af20c251e583057d3d4a0d8f4381fcd3a5ef8483
Derek Root <dkwroot@users.noreply.github.com>
2019-11-05 04:15:44 -0600

    added build assets

---
+++ client/build/precache-manifest.26e3eaff8dd942b768dbeece30b73907.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938a68ca8e8759186addf2ee0d2d6a1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "053cdbcabd060c637406",
+    "url": "/static/css/main.1a8127cc.chunk.css"
+  },
+  {
+    "revision": "93b93120880b7ccd0007",
+    "url": "/static/js/2.cab83b57.chunk.js"
+  },
+  {
+    "revision": "053cdbcabd060c637406",
+    "url": "/static/js/main.bb64fb90.chunk.js"
+  },
+  {
+    "revision": "6f79729af8dece6908cf",
+    "url": "/static/js/runtime~main.ccefb148.js"
+  },
+  {
+    "revision": "71c0ae98f0309fae3b7a325c3a5b1b58",
+    "url": "/static/media/logo.71c0ae98.svg"
+  },
+  {
+    "revision": "60141d7bafcf34fa74946b3f749b59a0",
+    "url": "/static/media/smiley.60141d7b.svg"
+  }
+]);
