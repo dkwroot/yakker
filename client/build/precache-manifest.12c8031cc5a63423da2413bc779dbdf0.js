3d62ebb9cb7db77ca1b29e1308107dcc53a7fa0e
Derek Root <dkwroot@users.noreply.github.com>
2019-11-05 04:38:47 -0600

    updated deployment

---
+++ client/build/precache-manifest.12c8031cc5a63423da2413bc779dbdf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21abd3956134c1b8650d373aac3219c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3571407b3b8d48c327d",
+    "url": "/static/css/main.1a8127cc.chunk.css"
+  },
+  {
+    "revision": "aea6b05ece24d90d996f",
+    "url": "/static/js/2.69b318d7.chunk.js"
+  },
+  {
+    "revision": "b3571407b3b8d48c327d",
+    "url": "/static/js/main.28db724d.chunk.js"
+  },
+  {
+    "revision": "6f79729af8dece6908cf",
+    "url": "/static/js/runtime~main.ccefb148.js"
+  },
+  {
+    "revision": "71c0ae98f0309fae3b7a325c3a5b1b58",
+    "url": "/static/media/logo.71c0ae98.svg"
+  },
+  {
+    "revision": "60141d7bafcf34fa74946b3f749b59a0",
+    "url": "/static/media/smiley.60141d7b.svg"
+  }
+]);
